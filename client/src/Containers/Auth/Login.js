e6e9590e8c67fc1a99a8c788f88a2a318a3fce4c
aryallaxman99 <aryallaxman99@gmail.com>
2023-05-18 23:58:37 +0545

    login page created

--- client/src/Containers/Auth/Login.js
+++ client/src/Containers/Auth/Login.js
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
-    <div>
-      <p>Login</p>
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-40">
+        <h1 className="mt-4xl text-center mb-4">Welcome to CommunialStay</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="primary">Login</button>
+          <div className="text-center py-2" text-gray-500>
+            Don't have account yet?{" "}
+            <Link className="underline text-black" to={"/register"}>
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
