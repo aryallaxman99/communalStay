5e8ba0496f8f1c7c1d459e6bf22bc7e4dc2e523e
aryallaxman99 <aryallaxman99@gmail.com>
2023-07-09 20:48:29 +0545

    places are shown in map

--- client/src/Components/maps/PlaceAddressPlot.js
+++ client/src/Components/maps/PlaceAddressPlot.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  InfoWindow,
+  LoadScript,
+  Marker,
+} from "@react-google-maps/api";
 import { toast } from "react-toastify";
 import axios from "axios";
 import requests from "../../Requests";
@@ -12,12 +17,11 @@ const PlaceAddressPlot = () => {
     lat: 27.700001,
     lng: 85.333336,
   });
-  const [place, setPlace] = useState();
-
-  console.log(place);
-  useEffect(() => {
-    axios.get(requests.getAllPlaces).then((res) => setPlace(res.data));
-  }, []);
+  const [place, setPlace] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [markers, setMarkers] = useState(null);
+  const [activeMarker, setActiveMarker] = useState(null);
+  const arr = [];
 
   const getCurrentLocation = () => {
     const success = (position) => {
@@ -38,17 +42,76 @@ const PlaceAddressPlot = () => {
     });
   };
 
+  if (place && loading) {
+    place.map(async (items) => {
+      await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${items.address}&key=${googleMapsApiKey}`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((jsonData) => {
+          if (jsonData.results[0].geometry.location) {
+            arr.push({
+              id: items._id,
+              name: items.title,
+              position: jsonData.results[0].geometry.location,
+            });
+          }
+          setMarkers(arr);
+          setLoading(false);
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    });
+  }
+
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
+
+  const getDirection = () => {
+    console.log("directions");
+  };
+
+  useEffect(() => {
+    axios.get(requests.getAllPlaces).then((res) => setPlace(res.data));
+  }, []);
+
   return (
-    <div className="flex">
+    <div className="flex gap-2">
       <div className="border relative">
         <LoadScript googleMapsApiKey={googleMapsApiKey}>
           <div className="relative">
             <GoogleMap
+              onClick={() => setActiveMarker(null)}
               mapContainerStyle={{ height: "600px", width: "800px" }}
               center={center}
               zoom={10}
-            ></GoogleMap>
-            <Button className="absolute bottom-48 right-3 bg-white rounded-sm w-auto">
+            >
+              {markers &&
+                markers.map(({ id, name, position }) => (
+                  <Marker
+                    key={id}
+                    position={position}
+                    onClick={() => handleActiveMarker(id)}
+                  >
+                    {activeMarker === id ? (
+                      <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                        <div onClick={() => getDirection()}>{name}</div>
+                      </InfoWindow>
+                    ) : null}
+                  </Marker>
+                ))}
+            </GoogleMap>
+            <Button
+              type="button"
+              className="absolute bottom-48 right-3 bg-white rounded-sm w-auto"
+            >
               <BiCurrentLocation
                 className="h-6 w-6 text-current hover:text-gray-700"
                 onClick={() => getCurrentLocation()}
