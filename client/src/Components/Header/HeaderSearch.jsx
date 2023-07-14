7d4dafef3169b5f1a6afd82672aa69473282977a
aryallaxman99 <aryallaxman99@gmail.com>
2023-07-14 21:20:37 +0545

    making search box UI

--- client/src/Components/Header/HeaderSearch.jsx
+++ client/src/Components/Header/HeaderSearch.jsx
@@ -1,8 +1,11 @@
 import { HiOutlineSearch } from "react-icons/hi";
 import { Link } from "react-router-dom";
-import Button from "../../widgets/button/Button";
+import Input from "../../widgets/input/Input";
+import { useState } from "react";
 
 const HeaderSearch = () => {
+  const [searchText, setSearchText] = useState("");
+  console.log(searchText);
   return (
     <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md -300">
       <Link to="/places/map" className="font-bold text-gray-700 cursor-pointer">
@@ -14,10 +17,17 @@ const HeaderSearch = () => {
       </Link>
 
       <div className="border border-left border-gray-300"></div>
-      <div>Any guest</div>
-      <Button className="bg-primary w-auto rounded-full">
-        <HiOutlineSearch className="w-4 h-4" />
-      </Button>
+      <div className="flex">
+        <Input
+          type="search"
+          className="z-10 w-24 h-5 rounded-full border-hidden text-gray-500 focus:w-full focus:h-7"
+          placeholder="Search..."
+          onKeyUp={(event) => setSearchText(event.target.value)}
+        />
+        <div className="bg-primary p-2 text-white w-auto rounded-full">
+          <HiOutlineSearch className="w-4 h-4" />
+        </div>
+      </div>
     </div>
   );
 };
