70ad305fb692e24a1a3eca4fd206caa9dc19840a
aryallaxman99 <aryallaxman99@gmail.com>
2023-07-21 20:38:58 +0545

    removing react-router-dom Outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,12 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import { UserContextProvider } from "./UserContext";
 import { ToastContainer } from "react-toastify";
-import { Error } from "./Containers/Pages/Error";
 import Layout from "./Containers/Layout/Layout";
-import Login from "./Containers/Auth/Login";
-import IndexPage from "./Containers/IndexPage";
-import Register from "./Containers/Auth/Register";
-import UserDashboard from "./Containers/User/UserDashboard";
-import Places from "./Containers/Places/Places";
-import PlacesForm from "./Containers/Places/PlacesForm";
-import PlacePage from "./Containers/Places/PlacePage";
-import BookingPage from "./Containers/Places/BookingPage";
-import ChangePassword from "./utils/ChangePassword";
-import OTP from "./Containers/ResetPassword/OTP";
-import EmailIdentification from "./Containers/ResetPassword/EmailIdentification";
-import PlaceAddressPlot from "./Components/maps/PlaceAddressPlot";
-import AnyWeekPlaces from "./Containers/Places/AnyWeekPlaces";
-import SearchResultPage from "./Containers/Places/SearchResultPage";
 
 const App = () => {
   return (
     <UserContextProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="*" element={<Error />} />
-          <Route index element={<IndexPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="identify" element={<EmailIdentification />} />
-          <Route path="/identify/otp" element={<OTP />} />
-          <Route path="/resetPassword" element={<ChangePassword />} />
-          <Route path="/account" element={<UserDashboard />} />
-          <Route path="/account/password" element={<ChangePassword />} />
-          <Route path="/account/places" element={<Places />} />
-          <Route path="/account/places/new" element={<PlacesForm />} />
-          <Route path="/account/places/:id" element={<PlacesForm />} />
-          <Route path="/listings/:id" element={<PlacePage />} />
-          <Route path="/account/bookings" element={<BookingPage />} />
-          <Route path="/places/map" element={<PlaceAddressPlot />} />
-          <Route path="/place/anyweek" element={<AnyWeekPlaces />} />
-          <Route path="/search" element={<SearchResultPage />} />
-        </Route>
-      </Routes>
+      <Layout />
       <ToastContainer position="top-center" autoClose={2000} />
     </UserContextProvider>
   );
--- client/src/Containers/Layout/Layout.js
+++ client/src/Containers/Layout/Layout.js
@@ -1,15 +1,54 @@
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "../../Components/Header/Header";
+import { Error } from "../Pages/Error";
+import IndexPage from "../IndexPage";
+import Login from "../Auth/Login";
+import Register from "../Auth/Register";
+import EmailIdentification from "../ResetPassword/EmailIdentification";
+import OTP from "../ResetPassword/OTP";
+import ChangePassword from "../../utils/ChangePassword";
+import UserDashboard from "../User/UserDashboard";
+import Places from "../Places/Places";
+import PlacesForm from "../Places/PlacesForm";
+import PlacePage from "../Places/PlacePage";
+import BookingPage from "../Places/BookingPage";
+import PlaceAddressPlot from "../../Components/maps/PlaceAddressPlot";
+import AnyWeekPlaces from "../Places/AnyWeekPlaces";
+import SearchResultPage from "../Places/SearchResultPage";
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <div className="app-height px-24">
-        <Outlet />
+        <UserRoutes />
       </div>
     </div>
   );
 };
 
 export default Layout;
+
+export const UserRoutes = () => {
+  return (
+    <Routes>
+      <Route path="*" element={<Error />} />
+      <Route index element={<IndexPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="identify" element={<EmailIdentification />} />
+      <Route path="/identify/otp" element={<OTP />} />
+      <Route path="/resetPassword" element={<ChangePassword />} />
+      <Route path="/account" element={<UserDashboard />} />
+      <Route path="/account/password" element={<ChangePassword />} />
+      <Route path="/account/places" element={<Places />} />
+      <Route path="/account/places/new" element={<PlacesForm />} />
+      <Route path="/account/places/:id" element={<PlacesForm />} />
+      <Route path="/listings/:id" element={<PlacePage />} />
+      <Route path="/account/bookings" element={<BookingPage />} />
+      <Route path="/places/map" element={<PlaceAddressPlot />} />
+      <Route path="/place/anyweek" element={<AnyWeekPlaces />} />
+      <Route path="/search" element={<SearchResultPage />} />
+    </Routes>
+  );
+};
