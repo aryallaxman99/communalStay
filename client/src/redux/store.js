43d512574150d36abcab54e59e8a089f7058de9c
aryallaxman99 <aryallaxman99@gmail.com>
2023-07-27 23:38:10 +0545

    redux error solved

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -17,7 +17,10 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
